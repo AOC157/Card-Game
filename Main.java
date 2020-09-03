5065a2f8bba8f1d4611f513c6612d7c9c43b371c
Hatef Zamany <hatef.zamany@yahoo.com>
2020-09-03 04:33:43 -0700

    declare king

--- Main.java
+++ Main.java
@@ -3,11 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Player you;
-        Player player1;
-        Player player2;
-        Player player3;
+        Game game = new Game();
 
+        Player you = new Player();
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
 
+        switch (game.declareHakem()){
+            case 0:
+                game.hakem = you;
+                break;
+            case 1:
+                game.hakem = player1;
+                break;
+            case 2:
+                game.hakem = player2;
+                break;
+            case 3:
+                game.hakem = player3;
+                break;
+        }
     }
 }
