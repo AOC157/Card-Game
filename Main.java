a204b9d0e0a1e2ddd64b6f3d86df228da2906802
Hatef Zamany <hatef.zamany@yahoo.com>
2020-09-03 04:05:01 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Player you;
+        Player player1;
+        Player player2;
+        Player player3;
+
+
+    }
+}
