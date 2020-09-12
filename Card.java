2bce595ba496567dff859b4a170154cb2bfe8a7b
Hatef Zamany <hatef.zamany@yahoo.com>
2020-09-12 05:59:24 -0700

    add isBigger function

--- Card.java
+++ Card.java
@@ -8,4 +8,11 @@ public class Card {
         this.type = type;
         this.number = number;
     }
+
+    public boolean isBigger(Card card, char kingCard) {
+        if(type == card.type){
+            return number > card.number;
+        }
+        return card.type != kingCard;
+    }
 }
